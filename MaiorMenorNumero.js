336b413eb22cdbeaf39dd2525353d063d5c96ebd
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2024-08-30 10:37:42 -0300

    Atualizando

---
+++ MaiorMenorNumero.js
@@ -0,0 +1,17 @@
+let numero=0,maior=0,menor=0,soma=0;
+
+for(i=0; i<10 ;i++){
+    numero = +(prompt("Digite um valor:"))
+    soma += numero
+
+    if(numero > maior){
+      maior = numero
+    }
+    if(numero < menor == 0){
+      menor = numero
+    }
+}
+
+document.write(`maior numero:${maior}`)
+document.write(`menor numero:${menor}`)
+document.write(`soma:${soma}`)
